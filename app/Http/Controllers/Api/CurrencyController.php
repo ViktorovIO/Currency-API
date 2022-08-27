c25831f04ab5d8192357c8262e933a940fa617eb
ViktorovIO <viktorovio@yahoo.com>
2022-08-27 15:38:31 +0500

    add Currency && CurrencyValue models

---
+++ app/Http/Controllers/Api/CurrencyController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Currency;
+use Illuminate\Http\Request;
+
+class CurrencyController extends Controller
+{
+    public function list()
+    {
+        return Currency::all();
+    }
+}
