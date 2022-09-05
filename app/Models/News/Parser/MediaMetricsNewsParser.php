b11603131b646dc0fb940b08c8c2e70a73859587
ViktorovIO <viktorovio@yahoo.com>
2022-09-06 01:18:55 +0500

    #8 MediaMetrics news parser

--- app/Models/News/Parser/MediaMetricsNewsParser.php
+++ app/Models/News/Parser/MediaMetricsNewsParser.php
@@ -3,6 +3,7 @@
 namespace App\Models\News\Parser;
 
 use App\Models\News\Dto\NewsDto;
+use PhpQuery\PhpQuery;
 
 class MediaMetricsNewsParser implements NewsParserInterface
 {
@@ -12,6 +13,24 @@ class MediaMetricsNewsParser implements NewsParserInterface
      */
     public function parse(string $contentString): array
     {
-        return [];
+        $result = [];
+        $phpQuery = new PhpQuery();
+        $phpQuery->load_str($contentString);
+
+        foreach ($phpQuery->query('.rs-link') as $newsItem) {
+            $name = $link = '';
+            $newsItemString = $phpQuery->innerHTML($newsItem);
+            if (preg_match("!href=\"(.*?)\">!si", $newsItemString, $matches)) {
+                $link = $matches[1];
+            }
+
+            if (preg_match("!\">(.*?)</a>!si", $newsItemString, $matches)) {
+                $name = $matches[1];
+            }
+
+            $result[] = new NewsDto($name, $link);
+        }
+
+        return $result;
     }
 }
